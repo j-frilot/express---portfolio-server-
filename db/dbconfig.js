034846d205810a6b1d043e5b8a39d58c5d20ce3f
j-frilot <frilotdr@gmail.com>
2021-05-28 15:43:02 -0500

    initial routes

---
+++ db/dbconfig.js
@@ -0,0 +1,19 @@
+// require("dotenv").config();
+// const mysql = require("mysql2");
+
+// // create the connection to database
+// const conn = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME
+// });
+// conn.connect((err) => {
+//     if (err) {
+//         console.log("Connection error message: " + err.message);
+//         return;
+//     }
+//     console.log("Connected!");
+// });
+
+// module.exports = conn;
